c07817325912157ce2e584badd645d570af2f6e6
memetibryan <memetibryan@gmail.com>
2018-03-08 18:03:38 +0300

    Beautified the code

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,27 +1,26 @@
 class IndexController < ApplicationController
 	def index
 		require 'net/http'
-require 'net/https'
-require 'uri'
+		require 'net/https'
+		require 'uri'
 
-uri = URI('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate')
+		uri = URI('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate')
 
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-request = Net::HTTP::Get.new(uri)
-request["accept"] = 'application/json'
-request["content-type"] = 'application/json'
-request["authorization"] = 'Bearer 9fAAXHt9Olgs0Lt62mYo0FVk5mFV'
-request.body = "{ \"ShortCode\":\" \",
-  \"CommandID\":\"CustomerPayBillOnline\",
-  \"Amount\":\" \",
-  \"Msisdn\":\" \",
-  \"BillRefNumber\":\" \" }"
+		request = Net::HTTP::Get.new(uri)
+		request["accept"] = 'application/json'
+		request["content-type"] = 'application/json'
+		request["authorization"] = 'Bearer 9fAAXHt9Olgs0Lt62mYo0FVk5mFV'
+		request.body = "{ \"ShortCode\":\" \",
+  			\"CommandID\":\"CustomerPayBillOnline\",
+  			\"Amount\":\" \",
+  			\"Msisdn\":\" \",
+  			\"BillRefNumber\":\" \" }"
 
-response = http.request(request)
-puts response.read_body
+		response = http.request(request)
 
 		json_response(response.read_body)
 	end
