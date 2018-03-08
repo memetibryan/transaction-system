82c5386d73c2e21cef7a98ed196a035b8450e4b6
memetibryan <memetibryan@gmail.com>
2018-03-08 17:11:29 +0300

    incorporated mpesa C2B API

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,25 +1,28 @@
 class IndexController < ApplicationController
 	def index
 		require 'net/http'
-require 'net/https'
-require 'uri'
+		require 'net/https'
+		require 'uri'
 
-uri = URI('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl')
+		uri = URI('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl')
 
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-request = Net::HTTP::Get.new(uri)
-request["accept"] = 'application/json'
-request["content-type"] = 'application/json'
-request["authorization"] = 'Bearer vpJ7Xd64sjGaUa4z8lcROXDPFTHo'
-request.body = "{\"ShortCode\":\"ShortCode\",
-    \"ResponseType\":\"ResponseType\",
-    \"ConfirmationURL\":\"http://ip_address:port/confirmation\",
-    \"ValidationURL\":\"http://ip_address:port/validation_url\"}"
+		request = Net::HTTP::Get.new(uri)
+		request["accept"] = 'application/json'
+		request["content-type"] = 'application/json'
+		request["authorization"] = 'Bearer f1lRsA21I4t3Q70irZwZjLZ5d9Ue'
+		request.body = "{\"ShortCode\":\"ShortCode\",
+    		\"ResponseType\":\"ResponseType\",
+    		\"ConfirmationURL\":\"http://ip_address:port/confirmation\",
+    		\"ValidationURL\":\"http://ip_address:port/validation_url\"}"
 
-response = http.request(request)
-puts response.read_body
+  		response = Net::HTTP.new(uri.host,uri.port) do |http|
+  			http.request(request)
+		end
+
+		json_response(request.body)
 	end
 end
