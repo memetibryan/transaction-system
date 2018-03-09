f6b509cdd6e7526bc7d3aa1d786a57c8d99c189e
memetibryan <memetibryan@gmail.com>
2018-03-09 22:33:49 +0300

    created transactions table in the database

---
+++ db/migrate/20180309193049_transaction.rb
@@ -0,0 +1,4 @@
+class Transaction < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309192347) do
+ActiveRecord::Schema.define(version: 20180309193049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20180309192347) do
     t.bigint "number"
     t.text "email"
     t.bigint "account_balance"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.text "sender"
+    t.text "recepient"
+    t.text "reference_number"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
