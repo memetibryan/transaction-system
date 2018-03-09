ceea53931cc2d52faacbc7b08575c0361e0de9eb
memetibryan <memetibryan@gmail.com>
2018-03-09 20:56:27 +0300

    added devise authentication configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,20 @@
 Rails.application.configure do
+
+  #devise authentication and mailing
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "infomemeti2@gmail.com",
+    password: "memetime"
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
